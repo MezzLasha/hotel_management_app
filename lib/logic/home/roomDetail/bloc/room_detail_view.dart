5ca262f569ab0bc96e44832b54275eddf063bed6
MezzLasha <mezzlasha@gmail.com>
2022-10-26 18:01:23 +0400

    room detail ui upgrade

--- lib/logic/home/roomDetail/bloc/room_detail_view.dart
+++ lib/logic/home/roomDetail/bloc/room_detail_view.dart
@@ -2,7 +2,7 @@ import 'package:animations/animations.dart';
 import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_villains/villain.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hotel_management_app/logic/home/models/room.dart';
 import 'package:hotel_management_app/logic/home/roomDetail/chat/bloc/room_chat_view.dart';
@@ -20,7 +20,8 @@ class RoomDetail extends StatefulWidget {
 
 class _RoomDetailState extends State<RoomDetail> {
   late Widget roomChat;
-
+  final _editButtonKey = GlobalKey<_ToggleEditButtonState>();
+  final _formKey = GlobalKey<FormState>();
   @override
   void initState() {
     roomChat = RoomChat(room: widget.room);
@@ -49,49 +50,143 @@ class _RoomDetailState extends State<RoomDetail> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _buildHeroBanner(context),
+        const Center(child: DragHandlePill()),
         Container(
-          height: 15,
+          height: 20,
         ),
         Container(
           margin: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'სტატუსი: ${widget.room.status == '2' ? 'თავისუფალი' : widget.room.status == '1' ? 'დაკავებული' : widget.room.status}',
-                style: GoogleFonts.inter(
-                    fontWeight: FontWeight.w600, fontSize: 16),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                'მფლობელი: ${widget.room.owner}',
-                style: GoogleFonts.inter(
-                    fontWeight: FontWeight.w600, fontSize: 16),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                'ჯგუფი: ${widget.room.group}',
-                style: GoogleFonts.inter(
-                    fontWeight: FontWeight.w600, fontSize: 16),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                enabled: false,
-                initialValue: widget.room.info,
-                decoration: const InputDecoration(
-                    labelText: 'კომენტარი', border: OutlineInputBorder()),
-              ),
-            ],
-          ),
+          child: _buildEditableForm(context),
         ),
       ],
     ));
+  }
+
+  Widget _buildEditableForm(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FormBuilderRadioGroup(
+              name: 'status',
+              enabled: false,
+              initialValue: widget.room.status == '0'
+                  ? 'თავისუფალი'
+                  : widget.room.status == '1'
+                      ? 'დაკავებული'
+                      : widget.room.status == '2'
+                          ? 'დასუფთავება'
+                          : widget.room.status,
+              options: const [
+                FormBuilderFieldOption(
+                  value: 'თავისუფალი',
+                ),
+                FormBuilderFieldOption(
+                  value: 'დაკავებული',
+                ),
+                FormBuilderFieldOption(
+                  value: 'დასუფთავება',
+                )
+              ],
+              decoration: const InputDecoration(
+                  labelText: 'სტატუსი', border: OutlineInputBorder())),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            enabled: false,
+            initialValue: widget.room.info,
+            decoration: const InputDecoration(
+                labelText: 'ინფორმაცია', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            enabled: false,
+            initialValue: widget.room.owner,
+            decoration: const InputDecoration(
+                labelText: 'მფლობელი', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            enabled: false,
+            initialValue: widget.room.group,
+            decoration: const InputDecoration(
+                labelText: 'ჯგუფი', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            enabled: false,
+            initialValue: widget.room.info,
+            decoration: const InputDecoration(
+                labelText: 'კომენტარი', border: OutlineInputBorder()),
+          ),
+          const SizedBox(
+            height: 180,
+          ),
+          SizedBox(
+            height: 50,
+            child: MyOutlineButton(
+                onPressed: () {
+                  showMyBottomDialog(
+                      context,
+                      [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          height: MediaQuery.of(context).size.height * 0.6,
+                          child: Text(
+                            widget.room
+                                .toString()
+                                .substring(5, widget.room.toString().length - 1)
+                                .replaceAll(',', '\n')
+                                .replaceAll(' ', ''),
+                            style: GoogleFonts.inter(fontSize: 18),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              height: 50,
+                              width: 70,
+                              child: MyOutlineButton(
+                                child: const Text('OK'),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                              child: MyOutlineButton(
+                                icon: const Icon(
+                                  Icons.copy,
+                                  size: 18,
+                                ),
+                                child: const Text('COPY'),
+                                onPressed: () async {
+                                  await Clipboard.setData(ClipboardData(
+                                      text: widget.room.toString()));
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                      GlobalKey());
+                },
+                child: const Text('JSON')),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 
   Hero _buildHeroBanner(BuildContext context) {
@@ -103,7 +198,7 @@ class _RoomDetailState extends State<RoomDetail> {
       child: Container(
         height: 100,
         width: double.infinity,
-        margin: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(20),
         decoration: BoxDecoration(
             borderRadius:
                 SmoothBorderRadius(cornerRadius: 5, cornerSmoothing: 0.6),
@@ -179,57 +274,36 @@ class _RoomDetailState extends State<RoomDetail> {
               style: GoogleFonts.inter(fontWeight: FontWeight.w500),
             ),
           ),
-          CircularButton(
-            icon: const Icon(Icons.info_outline),
-            onPressed: () {
-              showMyBottomDialog(
-                  context,
-                  [
-                    Container(
-                      alignment: Alignment.topLeft,
-                      height: MediaQuery.of(context).size.height * 0.6,
-                      child: Text(
-                        widget.room
-                            .toString()
-                            .substring(5, widget.room.toString().length - 1)
-                            .replaceAll(',', '\n')
-                            .replaceAll(' ', ''),
-                        style: GoogleFonts.inter(fontSize: 18),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          height: 50,
-                          width: 70,
-                          child: MyOutlineButton(
-                            child: const Text('OK'),
-                            onPressed: () => Navigator.pop(context),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 50,
-                          child: MyOutlineButton(
-                            icon: const Icon(
-                              Icons.copy,
-                              size: 18,
-                            ),
-                            child: const Text('COPY'),
-                            onPressed: () async {
-                              await Clipboard.setData(
-                                  ClipboardData(text: widget.room.toString()));
-                            },
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                  GlobalKey());
-            },
+          ToggleEditButton(
+            key: _editButtonKey,
           ),
         ],
       ),
+    );
+  }
+}
+
+class ToggleEditButton extends StatefulWidget {
+  const ToggleEditButton({super.key});
+
+  @override
+  State<ToggleEditButton> createState() => _ToggleEditButtonState();
+}
+
+class _ToggleEditButtonState extends State<ToggleEditButton> {
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularButton(
+      icon: _isSelected
+          ? const Icon(Icons.check)
+          : const Icon(Icons.edit_outlined),
+      onPressed: () {
+        setState(() {
+          _isSelected = !_isSelected;
+        });
+      },
     );
   }
 }
